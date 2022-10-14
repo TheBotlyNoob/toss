9366aeb1c1ba9190e151c012cd194fd9abafa433
Jay Jackson <jj@jj.is-a.dev>
2022-10-14 03:12:34 +0000

    move into different  dirs

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
-nasm %1.asm -o os
+nasm asm/%1.asm -o os
 qemu-system-x86_64 -nodefaults -boot order=c -vga std -drive file=os,format=raw,index=0,media=disk
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
-nasm "$1".asm -o os
+nasm asm/"$1".asm -o os
 qemu-system-x86_64 -nodefaults -boot order=c -vga std -drive file=os,format=raw,index=0,media=disk
